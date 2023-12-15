0538cf911e3a0331a354e0727827022d7d7b3779
nidh-eesh <nidhinmanu07@gmail.com>
2023-12-15 12:03:48 +0530

    feat: Added HTML table to button action

--- facilities_management_utility/public/js/quotation.js
+++ facilities_management_utility/public/js/quotation.js
@@ -26,3 +26,74 @@ frappe.ui.form.on('Quotation', {
         }
     }
 });
+
+frappe.ui.form.on('Quotation Item', {
+    custom_employee_details: function (frm) {
+        var employee_dialog = new frappe.ui.Dialog({
+            title: 'Table-Test',
+            size: "extra-large",
+            fields: [
+                {
+                    fieldtype: 'HTML',
+                    fieldname: 'table_section'
+                }
+            ],
+            // primary_action_label: 'Close',
+            // primary_action: function() {
+            //     employee_dialog.hide();
+            // }
+        });
+        // Define the HTML structure for the table
+        var tableHTML = `
+        <div>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>Employee</th>
+                        <th>Staff Contract salary</th>
+                        <th>Overtime</th>
+                        <th>Food</th>
+                        <th>Housing & Transport to/from work</th>
+                        <th>Visa/Transfer, Iqama & Labor card</th>
+                        <th>GOSI</th>
+                        <th>Medical Insurance (Class C)</th>
+                        <th>Vacation (21days/Year) / Gratuity (EOSB)</th>
+                        <th>Workers  Air Travel expenses</th>
+                        <th>Uniform</th>
+                        <th>Recruitment, Visa processing</th>
+                        <th>Exit Re entry</th>
+                        <th>Baladiya, Drivers License or Any other government registrations</th>
+                        <th>VAT. Taxes, COC, MOFA</th>
+                        <th>Quarantine or PCR Cost on Arrival</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>E1</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                        <td>Data 2</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        `;
+        employee_dialog.show();
+        // Set the HTML content of the field in the dialog
+        employee_dialog.fields_dict.table_section.$wrapper.html(tableHTML);
+    },
+});
+
+
