039d47c35e645a618965f93ee3e22f1dc66e2fd6
Jishna-tp <jishnatp7@gmail.com>
2023-12-16 11:41:49 +0530

    feat: Created doctype Auxiliary Quataion and Auxiliary Quotation Item

---
+++ facilities_management_utility/facilities_management_utility/doctype/auxiliary_quotation/auxiliary_quotation.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2023, efeone and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class AuxiliaryQuotation(Document):
+	pass
+
+@frappe.whitelist()
+def get_auxiliary_item(item_template):
+	print(item_template)
+	child_table_data = frappe.get_all('Auxiliary Item',filters={'parent': item_template},
+		fields=['item','rate']
+	)
+	return child_table_data
